46cab75c50406b91d5b779616183fd0175f3dbf3
cmta2112@gmail.com <Minita22>
2019-02-09 20:36:33 -0500

    toast_geoQuiz

--- GeoQuiz/app/src/main/java/pe/edu/cibertec/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/pe/edu/cibertec/geoquiz/MainActivity.java
@@ -2,6 +2,8 @@ package pe.edu.cibertec.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+        public void respCorrecta(View v){
+            Toast toast = Toast.makeText(this, "Respuesta Correcta", Toast.LENGTH_SHORT);
+            toast.show();
+
+
+    }
+
+    public void respIncorrecta(View v){
+        Toast toast = Toast.makeText(this, "Respuesta Incorrecta", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
 }
+
+
